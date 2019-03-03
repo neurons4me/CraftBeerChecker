ea04035bad72171283597492ed03e2d52083c5b6
neurons4me <adammcdaniel1@gmail.com>
2019-03-03 10:26:51 -0800

    Made early changes to generalize the scraping function to other breweries. Changed the function to take arguments instead of having it hard coded and added a decision point where the function decides which type of tags (class, ID, etc.) to pull from.

--- scraper.py
+++ scraper.py
@@ -8,13 +8,13 @@ import re
 import csv
 
 #TODO can make this more general. Give the general version target_url and brewery variables as inputs and have site specific cleaning/scraping steps confined to thier own functions (or use decorators?)
-def altamont_scraper():
+def altamont_scraper(target_url, brewery, name_element_type, name_element_name, style_element_type, style_element_name, abv_element_type, abv_element_name):
 
     option = webdriver.ChromeOptions()
     option.add_argument("incognito")
     option.add_argument("headless")
 
-    target_url = "https://altamontbeerworks.com/1/taproom/"
+    # target_url = "https://altamontbeerworks.com/1/taproom/"
 
     browser = webdriver.Chrome(executable_path="/home/adam/Documents/Projects/CraftBeerChecker/Utils/chromedriver", chrome_options=option)
 
@@ -27,16 +27,33 @@ def altamont_scraper():
         print("Timed out waiting for page to load")
         browser.quit()
 
-    beers_name_finder = browser.find_elements_by_class_name('beer-name')
-    beers_style_finder = browser.find_elements_by_class_name('beer-style')
-    beers_abv_finder = browser.find_elements_by_class_name('abv')
+    if name_element_type == 'class':
+        beers_name_finder = browser.find_elements_by_class_name(name_element_name)
+    elif name_element_type == 'other': #TODO fill out the other types of capture tags (ID, nth child of something etc. as determined when encountered)
+        pass
+
+    if style_element_type == 'class':
+        beers_style_finder = browser.find_elements_by_class_name(style_element_name)
+    elif style_element_type == 'other':
+        pass
+
+    if abv_element_type == 'class':
+        beers_abv_finder = browser.find_elements_by_class_name(abv_element_name)
+    elif abv_element_type == 'other':
+        pass
+
+
+
+    # beers_name_finder = browser.find_elements_by_class_name('beer-name')
+    # beers_style_finder = browser.find_elements_by_class_name('beer-style')
+    # beers_abv_finder = browser.find_elements_by_class_name('abv')
 
     beers_name = [x.text for x in beers_name_finder]
     beers_style = [x.text for x in beers_style_finder]
     beers_abv = [x.text for x in beers_abv_finder]
 
     today = str(datetime.date.today())
-    brewery = "Altamont"
+    # brewery = "Altamont"
 
     output_list = []
     if len(beers_name) == len(beers_style) and len(beers_style) == len(beers_abv):
@@ -62,4 +79,4 @@ def altamont_scraper():
 
     browser.quit()
 
-altamont_scraper()
+altamont_scraper("https://altamontbeerworks.com/1/taproom/", "Altamont", 'class', 'beer-name', 'class', 'beer-style', 'class', 'abv')
