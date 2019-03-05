074e0fd83c8e7d5ea27af35cf061389c1cb5a070
neurons4me <adammcdaniel1@gmail.com>
2019-03-04 19:01:33 -0800

    Added early general outline for novelty analysis. Added requirements.txt file for easier portability onto prod server (raspberry pi).

--- notifications.py
+++ notifications.py
@@ -1,1 +1,3 @@
 # This module houses functions for alerting the user of new beers or errors as well as basic logging
+
+import smtplib
--- novelty_checker.py
+++ novelty_checker.py
@@ -1,1 +1,16 @@
 # This module houses functions for reviewing the logs and checking for new beers
+
+import pandas as pd
+
+
+#algorithym steps
+# parameters (brewery)
+# 1. today_list = FROM brewery SELECT today
+# 2. yesterday_list = FROM brewery SELECT previous_day
+# 3. check for items on today list that are not on yesterday list
+# 4. append new beer to output list
+
+# datetime brewery_name -> beer_list
+
+def new_beers_today (brewery):
+    pass
